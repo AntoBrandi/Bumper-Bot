735a57a73bce2228bece7d17a4a079c7672eac06
AntoBrandi <antonio.brandi@outlook.it>
2025-10-22 22:45:56 +0200

    added RGB camera sim

--- bumperbot_description/launch/gazebo.launch.py
+++ bumperbot_description/launch/gazebo.launch.py
@@ -80,7 +80,9 @@ def generate_launch_description():
         arguments=[
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
             "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
-            "/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan"
+            "/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan",
+            "/image_raw@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
         ],
         remappings=[
             ('/imu', '/imu/out'),
